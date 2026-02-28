2065f21565d01b6463519b259466afcccc88bad4
Hayden Frank <44754643+haydenfrank@users.noreply.github.com>
2026-02-28 12:15:46 -0500

    butter dog base (#1)

--- Justfile
+++ Justfile
@@ -12,7 +12,7 @@ run: build
 # Default target (first recipe is default)
 build:
     rm -f {{target}}
-    {{zip}} -9 -r {{target}} main.lua conf.lua src gfx -x '*.git*' '*.DS_Store'
+    {{zip}} -9 -r {{target}} main.lua conf.lua src assets -x '*.git*' '*.DS_Store'
     echo Built {{target}}
 
 # Keep an `all` target for compatibility with the Makefile
--- main.lua
+++ main.lua
@@ -1,7 +1,30 @@
-function love.load() end
+function love.load()
+	ButterDog = {}
+	ButterDog.img = love.graphics.newImage("assets/butterdog.png")
+	ButterDog.x = 0
+	ButterDog.y = 0
+	ButterNova = love.audio.newSource("assets/butternova.ogg", "static")
+	ButterNova:setLooping(true)
+	ButterNova:play()
+	ButterNova:setVolume(0.25)
+end
 
 function love.keypressed() end
 
-function love.update() end
-
-function love.draw() end
+function love.update(dt)
+	if love.keyboard.isDown("d") then
+		ButterDog.x = ButterDog.x + 150 * dt
+	end
+	if love.keyboard.isDown("a") then
+		ButterDog.x = ButterDog.x - 150 * dt
+	end
+	if love.keyboard.isDown("w") then
+		ButterDog.y = ButterDog.y - 150 * dt
+	end
+	if love.keyboard.isDown("s") then
+		ButterDog.y = ButterDog.y + 150 * dt
+	end
+end
+function love.draw()
+	love.graphics.draw(ButterDog.img, ButterDog.x, ButterDog.y)
+end
